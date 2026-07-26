eb20c0a90b1632acb04396eb439f3b96555e4169
gingama4 <me@gingama4.com>
2026-07-26 18:01:29 +0900

    feat: 初期テンプレート状態でブログ作成

---
+++ src/blog/initial-post.md
@@ -0,0 +1,11 @@
+---
+title: "Hello World"
+pubDate: 2026-07-26
+description: "This is my first blog post on"
+tags: ["note"]
+---
+
+これは最初のテスト記事です。
+
+- Astroで再構築
+- Markdownで記事管理
---
+++ src/content.config.ts
@@ -0,0 +1,14 @@
+import { glob } from "astro/loaders";
+import { defineCollection } from "astro:content";
+import { z } from "astro/zod";
+
+const blog = defineCollection({
+  loader: glob({ pattern: "**[^_]*.md", base: "./src/blog" }),
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.date(),
+    tags: z.array(z.string()),
+  }),
+});
+
+export const collections = { blog };
